c425b5efd0c57b50261722b6745a7b721ad928b3
Amesh609 <99159628+Amesh609@users.noreply.github.com>
2023-09-16 13:16:21 -0700

    Upload all files related to this project

---
+++ javascript/carousel.js
@@ -0,0 +1,20 @@
+$('#client-reviews').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1.6,
+    vertical: true,
+    verticalSwiping: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+
+  $('#bp-carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  
+// End :: business partner section 
